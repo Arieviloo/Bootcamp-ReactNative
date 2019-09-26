6de288ced6b965e0cba942e2d915de1164d0d7ac
Arieviloo <jadieoliveira00@gmail.com>
2019-09-26 20:55:44 -0300

    Formulário

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
 import '../../config/ReactotronConfig';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container, Form, Input, SubmitButton } from './style';
 
 const Main = () => {
-  return <View />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar User"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 };
 
 Main.navigationOptions = {
-  title: 'Usuer',
+  title: 'Usuários',
 };
 
 export default Main;
